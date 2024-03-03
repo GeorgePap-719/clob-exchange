6c3008af49b525443d9524b8e2ff796a765b20e2
George Papadopoulos <george.719pap@gmail.com>
2024-03-03 22:46:35 +0200

    Update buildRun.sh

--- scripts/buildRun.sh
+++ scripts/buildRun.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cd ../
+# Set variable input_file to either $1 or /dev/stdin, in case $1 is empty
+# Note that this assumes that you are expecting the file name to operate on on $1
+input_file="${1:-/dev/stdin}"
 # -q is for running task in silent.
 ./gradlew clean build -q
-./gradlew run -q < test1.txt
+./gradlew run -q < "$input_file"
